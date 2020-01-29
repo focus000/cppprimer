1548c8c590eae33063af9beef256176d2ca16ff5
yunfangli <hanszimmerme@gmail.com>
2020-01-29 23:32:49 +0800

    add ex13_8

--- ch13/ex13_5_8.h
+++ ch13/ex13_5_8.h
@@ -1,11 +1,11 @@
 /*
  * @Author: liyunfang
  * @Date: 2020-01-29 22:48:24
- * @LastEditTime : 2020-01-29 22:56:24
+ * @LastEditTime : 2020-01-29 23:31:41
  * @Description: 
  */
-#ifndef EX13_5H
-#define EX13_5H
+#ifndef EX13_5_8H
+#define EX13_5_8H
 
 #include <string>
 using std::string;
@@ -15,6 +15,16 @@ class HasPtr
 public:
     HasPtr(string const &s = string()) : ps(new string(s)), i(0) {}
     HasPtr(HasPtr const &hp) : ps(new string(*hp.ps)), i(hp.i) {}
+    HasPtr &operator=(HasPtr const &rhs)
+    {
+        if (this != &rhs)
+        {
+            delete ps;
+            ps = new string(*rhs.ps);
+            i = rhs.i;
+        }
+        return *this;
+    }
 
 private:
     string *ps;
