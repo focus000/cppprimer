5b6a984283d77e2b588be678b255a21ca33dd145
yunfangli <hanszimmerme@gmail.com>
2020-01-30 17:04:01 +0800

    add ex3_11

--- ch13/ex13_5_8_11.h
+++ ch13/ex13_5_8_11.h
@@ -1,11 +1,11 @@
 /*
  * @Author: liyunfang
  * @Date: 2020-01-29 22:48:24
- * @LastEditTime : 2020-01-29 23:31:41
+ * @LastEditTime : 2020-01-30 17:02:42
  * @Description: 
  */
-#ifndef EX13_5_8H
-#define EX13_5_8H
+#ifndef EX13_5_8_11H
+#define EX13_5_8_11H
 
 #include <string>
 using std::string;
@@ -25,6 +25,7 @@ public:
         }
         return *this;
     }
+    ~HasPtr() {}
 
 private:
     string *ps;
