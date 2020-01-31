af5c41a78bf5ae2b24f45f1885c200cd575f6c43
yunfangli <hanszimmerme@gmail.com>
2020-01-31 22:44:44 +0800

    add ex13_30

--- ch13/ex13_5_8_11_22_30.h
+++ ch13/ex13_5_8_11_22_30.h
@@ -4,14 +4,16 @@
  * @LastEditTime : 2020-01-30 17:29:30
  * @Description: 
  */
-#ifndef EX13_5_8_11_22H
-#define EX13_5_8_11_22H
+#ifndef EX13_5_8_11_22_30H
+#define EX13_5_8_11_22_30H
 
+#include <iostream>
 #include <string>
 using std::string;
 
 class HasPtr
 {
+    friend void swap(HasPtr &, HasPtr &);
 public:
     HasPtr(string const &s = string()) : ps(new string(s)), i(0) {}
     HasPtr(HasPtr const &hp) : ps(new string(*hp.ps)), i(hp.i) {}
@@ -32,5 +34,14 @@ private:
     string *ps;
     int i;
 };
+
+inline
+void swap(HasPtr &lhs, HasPtr &rhs)
+{
+    using std::swap;
+    swap(lhs.ps, rhs.ps);
+    swap(lhs.i, rhs.i);
+    std::cout << "call swap(HasPtr& lhs, HasPtr& rhs)" << std::endl;
+}
 
 #endif
