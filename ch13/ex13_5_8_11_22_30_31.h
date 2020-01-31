e250c40aebd52266a7896cfaf174afb04a75b06a
yunfangli <hanszimmerme@gmail.com>
2020-01-31 23:17:17 +0800

    ex13_31 added

--- ch13/ex13_5_8_11_22_30_31.h
+++ ch13/ex13_5_8_11_22_30_31.h
@@ -4,8 +4,8 @@
  * @LastEditTime : 2020-01-30 17:29:30
  * @Description: 
  */
-#ifndef EX13_5_8_11_22_30H
-#define EX13_5_8_11_22_30H
+#ifndef EX13_5_8_11_22_30_31H
+#define EX13_5_8_11_22_30_31H
 
 #include <iostream>
 #include <string>
@@ -14,21 +14,23 @@ using std::string;
 class HasPtr
 {
     friend void swap(HasPtr &, HasPtr &);
+    friend bool operator<(HasPtr const &, HasPtr const &);
 public:
     HasPtr(string const &s = string()) : ps(new string(s)), i(0) {}
     HasPtr(HasPtr const &hp) : ps(new string(*hp.ps)), i(hp.i) {}
-    HasPtr &operator=(HasPtr const &rhs)
+    HasPtr &operator=(HasPtr rhs)
     {
-        if (this != &rhs)
-        {
-            auto tp = new string(*rhs.ps);
-            delete ps;
-            ps = tp;
-            i = rhs.i;
-        }
+        this->swap(rhs);
         return *this;
     }
     ~HasPtr() { delete ps; }
+    void swap(HasPtr &rhs)
+    {
+        using std::swap;
+        swap(ps, rhs.ps);
+        swap(i, rhs.i);
+        std::cout << "call swap(" << *rhs.ps << ")" << std::endl;
+    }
 
 private:
     string *ps;
@@ -38,10 +40,11 @@ private:
 inline
 void swap(HasPtr &lhs, HasPtr &rhs)
 {
-    using std::swap;
-    swap(lhs.ps, rhs.ps);
-    swap(lhs.i, rhs.i);
-    std::cout << "call swap(HasPtr& lhs, HasPtr& rhs)" << std::endl;
+    lhs.swap(rhs);
+}
+bool operator<(HasPtr const &lhs, HasPtr const &rhs)
+{
+    return (*lhs.ps == *rhs.ps) ? (lhs.i < rhs.i) : (*lhs.ps < *rhs.ps);
 }
 
 #endif
